8ef6de67ddafb191d3ed920741db292d39522f9c
Andrea Tomatis <a.tomatis.1009@vallauri.edu>
2020-04-02 14:57:59 +0200

    FUNZIONA

---
+++ Dias/Es05_PersonalTrainer/JS/Script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    //alert("gino");
+    visualizzaEsercizi();
+
+    $("#sceltatitoli").change(function(){
+      alert(this.value);
+    });
+
+
+  });
+
+  function visualizzaEsercizi() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("wrapper").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "visualizzaElenco.php", true);
+    xhttp.send();
+  } 
+
+  function aggiungiEsercizio(){
+    //alert(document.getElementById("titolo").value);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("wrapper").innerHTML += "Esercizio aggiunto con successo";
+      }
+    };
+    
+    var dati =  "?titolo='"+document.getElementById("titolo").value+"'&indice='"+document.getElementById("indice").value+"'&attività='"
+    +document.getElementById("attività").value+"'&descrizione='"+document.getElementById("descrizione").value+"'&ripetizioni='"
+    +document.getElementById("ripetizioni").value+"'&durata='"+document.getElementById("durata").value+"'&tempoRiposo='"
+    +document.getElementById("tempoRiposo").value+"'&media='"+document.getElementById("media").value+"'&file='"+document.getElementById("file").value+"'";
+    xhttp.open("GET", "aggiungi.php"+dati, true);
+    //visualizzaEsercizi();
+    xhttp.send();
+
+    
+  }
